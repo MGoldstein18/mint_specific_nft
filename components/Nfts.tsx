7f66618291476dffac87a00f942d0fd0ae86aa07
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-04-18 23:00:23 +0200

    fix(get-nfts): check for minted nfts from sdk

--- components/Nfts.tsx
+++ components/Nfts.tsx
@@ -1,4 +1,11 @@
-import { Box, SimpleGrid, Button, Flex, Image } from '@chakra-ui/react';
+import {
+  Box,
+  SimpleGrid,
+  Button,
+  Flex,
+  Image,
+  Heading,
+} from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useAddress, useNFTCollection, useMetamask } from '@thirdweb-dev/react';
 
@@ -7,6 +14,8 @@ const Nfts = () => {
   const [loading, setLoading] = useState(false);
   // State for nft metadata
   const [nftMetadata, setNftMetadata] = useState([null]);
+
+  const [fetchedNfts, setFetchedNfts] = useState(false);
 
   const fetchNfts = async () => {
     try {
@@ -18,6 +27,7 @@ const Nfts = () => {
       });
       const data = await response.json();
       setNftMetadata(data);
+      setFetchedNfts(true);
     } catch (error) {
       console.error(error);
     }
@@ -29,10 +39,9 @@ const Nfts = () => {
 
   // Use address and connect with metamask
   const address = useAddress();
-  const connectWithMetamask = useMetamask();
 
   // You can find your contract address in your dashboard after you have created an NFT Collection contract
-  const nftCollectionAddress = '0xB20B7AA964365A753Fb7412d98D76D139fd7Cbbd';
+  const nftCollectionAddress = '0x121923e1C44585d3e1417B1e3e7cE17be6546e7e';
 
   // Connect to contract using the address
   const nftCollection = useNFTCollection(nftCollectionAddress);
@@ -40,7 +49,6 @@ const Nfts = () => {
   // Function which generates signature and mints NFT
   const mintNft = async (id: number) => {
     try {
-      connectWithMetamask();
       setLoading(true);
       // Call API to generate signature and payload for minting
       const response = await fetch('/api/get-nfts', {
@@ -68,56 +76,60 @@ const Nfts = () => {
       console.log(error);
       alert('Failed to mint NFT!');
     }
-  }
+  };
 
-  return (
-    <SimpleGrid justifyItems='center' columns={3} spacing={10}>
-      {nftMetadata?.map((nft: any) => (
-        <Box
-          key={nftMetadata.indexOf(nft)}
-          maxW='sm'
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-        >
-          <Image src={nft?.url} alt='NFT image' />
+  if (fetchedNfts) {
+    return (
+      <SimpleGrid m='2rem' justifyItems='center' columns={3} spacing={10}>
+        {nftMetadata?.map((nft: any) => (
+          <Box
+            key={nftMetadata.indexOf(nft)}
+            maxW='sm'
+            borderWidth='1px'
+            borderRadius='lg'
+            overflow='hidden'
+          >
+            <Image src={nft?.url} alt='NFT image' />
 
-          <Flex p='1rem' alignItems='center' flexDir='column'>
-            <Box
-              mt='1'
-              fontWeight='bold'
-              lineHeight='tight'
-              fontSize='20'
-              isTruncated
-              m='0.5rem'
-            >
-              {nft?.name}
-            </Box>
-
-            <Box fontSize='16' m='0.5rem'>
-              {nft?.description}
-            </Box>
-            <Box fontSize='16' m='0.5rem'>
-              {nft?.price}
-            </Box>
-            {loading ? (
-              <p>Minting... You will need to approve 1 transaction</p>
-            ) : nft?.minted ? (
-              <b>This NFT has already been minted</b>
-            ) : (
-              <Button
-                colorScheme='purple'
+            <Flex p='1rem' alignItems='center' flexDir='column'>
+              <Box
+                mt='1'
+                fontWeight='bold'
+                lineHeight='tight'
+                fontSize='20'
+                isTruncated
                 m='0.5rem'
-                onClick={() => mintNft(nft?.id)}
               >
-                Mint
-              </Button>
-            )}
-          </Flex>
-        </Box>
-      ))}
-    </SimpleGrid>
-  );
+                {nft?.name}
+              </Box>
+
+              <Box fontSize='16' m='0.5rem'>
+                {nft?.description}
+              </Box>
+              <Box fontSize='16' m='0.5rem'>
+                {nft?.price}
+              </Box>
+              {loading ? (
+                <p>Minting... You will need to approve 1 transaction</p>
+              ) : nft?.minted ? (
+                <b>This NFT has already been minted</b>
+              ) : (
+                <Button
+                  colorScheme='purple'
+                  m='0.5rem'
+                  onClick={() => mintNft(nft?.id)}
+                >
+                  Mint
+                </Button>
+              )}
+            </Flex>
+          </Box>
+        ))}
+      </SimpleGrid>
+    );
+  } else {
+    return <Heading>Loading...</Heading>;
+  }
 };
 
 export default Nfts;
--- pages/api/get-nfts.ts
+++ pages/api/get-nfts.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+import {
+  ThirdwebSDK,
+  PayloadToSign721,
+  NFTMetadata,
+  NFTMetadataOwner,
+} from '@thirdweb-dev/sdk';
 import { ethers } from 'ethers';
 import dotenv from 'dotenv';
-const path = require('path');
-const { readFileSync, writeFileSync } = require('fs');
 
 dotenv.config();
 
@@ -45,10 +48,62 @@ export default async function handler(
       price: 0.03,
       minted: false,
     },
+    {
+      id: 3,
+      name: 'NFT 4',
+      description: 'This is our forth amazing NFT',
+      url: 'https://bafybeihgfxd5f5sqili34vyjyfai6kezlagrya43e6bkgw6hnxucxug5ya.ipfs.nftstorage.link/',
+      price: 0.01,
+      minted: false,
+    },
+    {
+      id: 4,
+      name: 'NFT 5',
+      description: 'This is our fifth amazing NFT',
+      url: 'https://bafybeihgfxd5f5sqili34vyjyfai6kezlagrya43e6bkgw6hnxucxug5ya.ipfs.nftstorage.link/',
+      price: 0.02,
+      minted: false,
+    },
+    {
+      id: 5,
+      name: 'NFT 6',
+      description: 'This is our sixth amazing NFT',
+      url: 'https://bafybeihgfxd5f5sqili34vyjyfai6kezlagrya43e6bkgw6hnxucxug5ya.ipfs.nftstorage.link/',
+      price: 0.03,
+      minted: false,
+    },
   ];
+
+  // Connect to thirdweb SDK
+  const sdk = new ThirdwebSDK(
+    new ethers.Wallet(
+      // Your wallet private key
+      process.env.PRIVATE_KEY as string,
+      // RPC URL
+      ethers.getDefaultProvider(
+        'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+      )
+    )
+  );
+
+  // Set variable for the NFT collection contract address which can be found after creating an NFT collection in the dashboard
+  const nftCollectionAddress = '0x121923e1C44585d3e1417B1e3e7cE17be6546e7e';
+
+  // Initialize the NFT collection with the contract address
+  const nftCollection = sdk.getNFTCollection(nftCollectionAddress);
 
   switch (req.method) {
     case 'GET':
+      const mintedNfts: NFTMetadataOwner[] = await nftCollection?.getAll();
+      if (!mintedNfts) {
+        res.status(200).json(nfts);
+      }
+      mintedNfts.forEach((nft) => {
+        if (nft.metadata.attributes) {
+          // @ts-ignore
+          nfts[nft.metadata.attributes.id].minted = true;
+        }
+      });
       res.status(200).json(nfts);
       break;
     case 'POST':
@@ -59,34 +114,17 @@ export default async function handler(
         res.status(400).json({ message: 'Invalid request' });
       }
 
-      // Connect to thirdweb SDK
-      const sdk = new ThirdwebSDK(
-        new ethers.Wallet(
-          // Your wallet private key
-          process.env.PRIVATE_KEY as string,
-          // RPC URL
-          ethers.getDefaultProvider(
-            'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
-          )
-        )
-      );
-
-      // Set variable for the NFT collection contract address which can be found after creating an NFT collection in the dashboard
-      const nftCollectionAddress = '0xB20B7AA964365A753Fb7412d98D76D139fd7Cbbd';
-
-      // Initialize the NFT collection with the contract address
-      const nftCollection = sdk.getNFTCollection(nftCollectionAddress);
-
       // Give 3 minutes to mint the NFT
       const startTime = new Date();
       const endTime = new Date(Date.now() + 180 * 1000);
 
       // Set up the NFT metadata
-      const metadata = {
+      const metadata: PayloadToSign721 = {
         metadata: {
           name: nfts[id].name,
           description: nfts[id].description,
           image: nfts[id].url,
+          attributes: { id },
         },
         price: nfts[id].price,
         mintStartTime: startTime,
@@ -96,11 +134,6 @@ export default async function handler(
 
       try {
         const response = await nftCollection?.signature.generate(metadata);
-
-        // Update the minted status of the NFT to true so that it can't be minted again
-        const newNFTs = nfts;
-        newNFTs[id].minted = true;
-        writeFileSync('data/nfts.json', JSON.stringify(newNFTs));
 
         res.status(201).json({
           payload: response?.payload,
